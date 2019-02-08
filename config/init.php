8def86dcbced849bc3a2f5b6d14a43a0ca4a6889
Anatoly Gorkov <gorkov.as@yandex.ru>
2019-02-09 02:24:57 +0400

    added initial configuration

---
+++ config/init.php
@@ -0,0 +1,28 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/gshop/core');
+define("LIBS", ROOT . '/gshop/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+// http://custom_shop/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+// http://custom_shop/public/
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+// http://custom_shop
+$app_path = str_replace('/public/', '', $app_path);
+
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
+
+
+
